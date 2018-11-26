9f4be874a0cafad499ac841d09a2aae39f37ce96
Ubuntu <ubuntu@ip-172-31-25-90.ec2.internal>
2018-11-26 20:57:30 +0000

    Updated download.sh file

--- tools/download.sh
+++ tools/download.sh
@@ -6,28 +6,28 @@ unzip data/glove.6B.zip -d data/glove
 rm data/glove.6B.zip
 
 # Questions
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Questions_Train_mscoco.zip
-unzip data/v2_Questions_Train_mscoco.zip -d data
-rm data/v2_Questions_Train_mscoco.zip
+wget -P data http://visualqa.org/data/abstract_v002/vqa/Questions_Train_abstract_v002.zip
+unzip data/Questions_Train_abstract_v002.zip -d data
+rm data/Questions_Train_abstract_v002.zip
 
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Questions_Val_mscoco.zip
-unzip data/v2_Questions_Val_mscoco.zip -d data
-rm data/v2_Questions_Val_mscoco.zip
+wget -P data http://visualqa.org/data/abstract_v002/vqa/Questions_Val_abstract_v002.zip
+unzip data/Questions_Val_abstract_v002.zip -d data
+rm data/Questions_Val_abstract_v002.zip
 
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Questions_Test_mscoco.zip
-unzip data/v2_Questions_Test_mscoco.zip -d data
-rm data/v2_Questions_Test_mscoco.zip
+wget -P data http://visualqa.org/data/abstract_v002/vqa/Questions_Test_abstract_v002.zip
+unzip data/Questions_Test_abstract_v002.zip -d data
+rm data/Questions_Test_abstract_v002.zip
 
 # Annotations
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Train_mscoco.zip
-unzip data/v2_Annotations_Train_mscoco.zip -d data
-rm data/v2_Annotations_Train_mscoco.zip
+wget -P data http://visualqa.org/data/abstract_v002/vqa/Annotations_Train_abstract_v002.zip
+unzip data/Annotations_Train_abstract_v002.zip -d data
+rm data/Annotations_Train_abstract_v002.zip
 
-wget -P data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Val_mscoco.zip
-unzip data/v2_Annotations_Val_mscoco.zip -d data
-rm data/v2_Annotations_Val_mscoco.zip
+wget -P data http://visualqa.org/data/abstract_v002/vqa/Annotations_Val_abstract_v002.zip
+unzip data/Annotations_Val_abstract_v002.zip -d data
+rm data/Annotations_Val_abstract_v002.zip
 
 # Image Features
-wget -P data https://imagecaption.blob.core.windows.net/imagecaption/trainval_36.zip
-unzip data/trainval_36.zip -d data
-rm data/trainval_36.zip
+# wget -P data https://imagecaption.blob.core.windows.net/imagecaption/trainval_36.zip
+# unzip data/trainval_36.zip -d data
+# rm data/trainval_36.zip
