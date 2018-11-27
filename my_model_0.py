bdf45cfcdd6ebb5ea1cd9cc736d54ab310733805
Belen Saldias <bcsaldias@uc.cl>
2018-11-27 03:42:13 +0000

    Renamed my model

--- my_model_0.py
+++ my_model_0.py
@@ -7,13 +7,14 @@ from fc import FCNet
 
 
 class BaseModel(nn.Module):
-    def __init__(self, w_emb, q_emb, v_att, q_net, v_net, classifier):
+    def __init__(self, w_emb, q_emb, v_att, q_net, v_net, u_net, classifier):
         super(BaseModel, self).__init__()
         self.w_emb = w_emb
         self.q_emb = q_emb
         self.v_att = v_att
         self.q_net = q_net
         self.v_net = v_net
+        self.u_net = u_net
         self.classifier = classifier
 
         
@@ -32,17 +33,17 @@ class BaseModel(nn.Module):
         w_emb = self.w_emb(q) # (A) words2vec
         q_emb = self.q_emb(w_emb) # (B) [batch, q_dim] questions2vec 
 
-        att = self.v_att(v, q_emb) # (C) returns weights for each location - improved attention Resize using attention
+        att = self.v_att(v, q_emb) # (C) improved attention
         v_emb = (att * v).sum(1) # (D) [batch, v_dim]
 
+        u_emb = v.sum(1) # ( ) [batch, v_dim] [TODO TODO]
         
         q_repr = self.q_net(q_emb) # (E) match size - QUESTION 
-
-        v_repr_td = self.v_net(v_emb) # (F) match size - IMG TOP DOWN ATTENTION
-        v_repr_bu = None              # ( ) match size - IMG BOTTOM UP ATTENTION [TODO TODO]
+        v_repr_td = self.v_net(v_emb) # (F) match size - IMG TOP DOWN
+        v_repr_bu = self.u_net(u_emb) # ( ) match size - IMG BOTTOM UP 
         
         joint_repr_td = q_repr * v_repr_td # (G)
-        joint_repr_bu = q_repr * v_repr_bu # ( ) [TODO TODO]
+        joint_repr_bu = q_repr * v_repr_bu # ( )
 
         joint_repr = torch.cat((joint_repr_td, joint_repr_bu), 1)
         
@@ -56,9 +57,10 @@ def build_baseline0(dataset, num_hid):
     v_att = Attention(dataset.v_dim, q_emb.num_hid, num_hid)
     q_net = FCNet([num_hid, num_hid])
     v_net = FCNet([dataset.v_dim, num_hid])
+    u_net = FCNet([dataset.v_dim, num_hid])
     classifier = SimpleClassifier(
         num_hid, 2 * num_hid, dataset.num_ans_candidates, 0.5)
-    return BaseModel(w_emb, q_emb, v_att, q_net, v_net, classifier)
+    return BaseModel(w_emb, q_emb, v_att, q_net, v_net, u_net, classifier)
 
 
 ######################## 
@@ -74,5 +76,5 @@ def build_baseline0_newatt(dataset, num_hid):
     v_net = FCNet([dataset.v_dim, num_hid]) # match dimensions
     
     classifier = SimpleClassifier(
-        num_hid, num_hid * 2, dataset.num_ans_candidates, 0.5)
+        num_hid *2 , num_hid * 2, dataset.num_ans_candidates, 0.5)
     return BaseModel(w_emb, q_emb, v_att, q_net, v_net, classifier)
