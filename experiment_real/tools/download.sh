dc58c63e7238e7acfbc45cb1c0efda84fd300249
Belen Saldias <bcsaldias@uc.cl>
2018-11-28 22:39:52 +0000

    Updated download real world images features

---
+++ experiment_real/tools/download.sh
@@ -0,0 +1,33 @@
+## Script for downloading data
+
+# GloVe Vectors
+wget -P experiment_real/data http://nlp.stanford.edu/data/glove.6B.zip
+unzip experiment_real/data/glove.6B.zip -d experiment_real/data/glove
+rm experiment_real/data/glove.6B.zip
+
+# Questions
+wget -P experiment_real/data http://visualqa.org/data/mscoco/vqa/v2_Questions_Train_mscoco.zip
+unzip experiment_real/data/v2_Questions_Train_mscoco.zip -d experiment_real/data
+rm experiment_real/data/v2_Questions_Train_mscoco.zip
+
+wget -P experiment_real/data http://visualqa.org/data/mscoco/vqa/v2_Questions_Val_mscoco.zip
+unzip experiment_real/data/v2_Questions_Val_mscoco.zip -d experiment_real/data
+rm experiment_real/data/v2_Questions_Val_mscoco.zip
+
+wget -P experiment_real/data http://visualqa.org/data/mscoco/vqa/v2_Questions_Test_mscoco.zip
+unzip experiment_real/data/v2_Questions_Test_mscoco.zip -d experiment_real/data
+rm experiment_real/data/v2_Questions_Test_mscoco.zip
+
+# Annotations
+wget -P experiment_real/data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Train_mscoco.zip
+unzip experiment_real/data/v2_Annotations_Train_mscoco.zip -d experiment_real/data
+rm experiment_real/data/v2_Annotations_Train_mscoco.zip
+
+wget -P experiment_real/data http://visualqa.org/data/mscoco/vqa/v2_Annotations_Val_mscoco.zip
+unzip experiment_real/data/v2_Annotations_Val_mscoco.zip -d experiment_real/data
+rm experiment_real/data/v2_Annotations_Val_mscoco.zip
+
+# Image Features
+wget -P experiment_real/data https://imagecaption.blob.core.windows.net/imagecaption/trainval_36.zip
+unzip experiment_real/data/trainval_36.zip -d experiment_real/data
+rm experiment_real/data/trainval_36.zip
